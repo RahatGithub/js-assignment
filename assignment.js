ef020ae5a296fd817d19f06e21355eab6535825e
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-01-10 19:50:01 +0600

    problem 2 done

--- assignment.js
+++ assignment.js
@@ -1,32 +1,38 @@
 
-//Problem no.1
-function kilometerToMeter(km){
-    if(km > 0){
+
+//******Problem no.1*******
+function kilometerToMeter(km) {
+    if (km > 0) {
         var m = km * 1000;
         return m
     }
-    else{
-        return 'please enter a valid input';
+    else {
+        return 'Enter a valid input';
     }
 }
 
-var readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-  readline.question('Kilometer : ', km => {
-    km = parseInt(km);
-    var result = kilometerToMeter(km);
-    console.log(result)
-    readline.close()
-  })
-  
+var km = 14;
+kilometerToMeter(km);
+
+
+
+//******Problem no.2*******
+function budgetCalculator(watches, phones, laptops) {
+    if (watches >= 0 && phones >= 0 && laptops >= 0) {
+        var totalCost = 0;
+        totalCost += (watches * 50 + phones * 100 + laptops * 500)
+        return totalCost;
+    }
+    else{
+        return 'Enter a valid input';
+    }
+}
+
+var watches = 14, phones = 12, laptops = 5;
+budgetCalculator(watches, phones, laptops);
 
 
 
 
-/*
-function budgetCalculator(watches, phones, laptops){
 
-}*/
+
